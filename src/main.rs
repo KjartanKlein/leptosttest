677e5124478032ff6b04bbc1f0db06c87d7fb0fd
Kjartan Bjarmi Árnason Klein <kjartanbklein@gmail.com>
2023-03-14 14:35:16 +0000

    Update main.rs
    added stuff from app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
 #[cfg(feature = "ssr")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -36,4 +40,48 @@ pub fn main() {
     // no client-side main function
     // unless we want this to work with e.g., Trunk for pure client-side testing
     // see lib.rs for hydration function instead
+}
+
+
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context(cx);
+
+    view! {
+        cx,
+
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+       <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+       // <Stylesheet id="leptos" href="style/main.scss"/>
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes>
+                    <Route path="" view=|cx| view! { cx, <HomePage/> }/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage(cx: Scope) -> impl IntoView {
+    // Creates a reactive value to update the button
+   // let (count, set_count) = create_signal(cx, 0);
+    //let on_click = move |_| set_count.update(|count| *count += 1);
+
+    view! { cx,
+        <div class="header">
+                <h1>"Armfield Test"</h1>
+        </div>
+        
+        //<button on:click=on_click>"Click Me: " {count}</button>
+    }
 }
